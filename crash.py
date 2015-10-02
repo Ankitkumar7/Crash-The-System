d5302ba61a081dcbc6c85ef2c84f96f671490061
Ankit kumar <ankit.kumar@btech.christuniversity.in>
2015-10-02 06:27:31 +0100

    Crash.py
    After Saving this .py file , convert the .py to .exe by using py2exe

---
+++ crash.py
@@ -0,0 +1,6 @@
+# Crash-The-System
+
+import webbrowser
+for i in range(20):
+    webbrowser.open('https://www.youtube.com/watch?v=nMUTEbcDgr8&feature=youtu.be')
+    print "Your system is going to crashed !"
